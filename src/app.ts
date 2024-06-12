a285a98f9eb6105b3d52ab36a2ff4af9c3e88fc1
Lugeja <aaronalev@gmail.com>
2024-06-12 22:40:45 +0300

    Create new Todo from user data

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
-import express, {Request, Response, NextFunction} from 'express'
+import express from 'express'
+import {Request, Response, NextFunction} from 'express'
 import todoRoutes from './routes/todos'
+import { json } from 'body-parser'
 import { error } from 'console'
 
 const app = express()
 
+app.use(json())
 app.use('/todos', todoRoutes)
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.json({message: error.message})
 })
 
-app.listen(25565, () => {
-    console.log('Suspicious activity detected on port 25565')
+app.listen(3011, () => {
+    console.log('Suspicious activity detected on port 3011')
 })
